e109508b0dae5b6c36333f97c8a04ca2ee5cfc89
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2025-01-21 13:43:51 -0500

    Update authentication.php

--- core/authentication/resources/classes/authentication.php
+++ core/authentication/resources/classes/authentication.php
@@ -141,6 +141,9 @@ syslog(LOG_WARNING, '1 _SESSION[authentication][plugin][]: '.print_r($_SESSION['
 					$_SESSION['authentication']['plugin'][$name]['user_email'] = $_SESSION['user_email'];
 					$_SESSION['authentication']['plugin'][$name]['authorized'] = 0;
 				}
+				elseif ($_SESSION['authentication']['plugin'][$name]['authorized'] == true){
+					$result = ['authentication']['plugin'][$name];
+				}
 			}
 syslog(LOG_WARNING, '2 _SESSION[authentication][plugin][]: '.print_r($_SESSION['authentication']['plugin'], true));
 		//debug information
@@ -152,7 +155,6 @@ syslog(LOG_WARNING, '2 _SESSION[authentication][plugin][]: '.print_r($_SESSION['
 				foreach($_SESSION['authentication']['plugin'] as $row) {
 					if ((bool)$row["authorized"]) {
 						$authorized |= true;
-						$result = $row;
 					}
 					else {
 						$authorized |= false;
