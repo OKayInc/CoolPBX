1702d9ff1a5e653c57ac36f78ed1cd1e1efd9a69
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2025-03-13 11:37:42 -0400

    Update app_config.php

--- app/time_conditions/app_config.php
+++ app/time_conditions/app_config.php
@@ -44,6 +44,7 @@
 		$apps[$x]['destinations'][$y]['field']['description'] = "dialplan_description";
 		$apps[$x]['destinations'][$y]['select_value']['dialplan'] = "transfer:\${destination} XML \${context}";
 		$apps[$x]['destinations'][$y]['select_value']['ivr'] = "menu-exec-app:transfer \${destination} XML \${context}";
+		$apps[$x]['destinations'][$y]['select_value']['simple'] = "\${destination}";
 		$apps[$x]['destinations'][$y]['select_label'] = "\${destination} \${name} \${description}";
 
 	//permission details
