d0d6f435865cc141437256ab14c99dc1e74b3bb5
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2025-03-13 11:37:05 -0400

    Update app_config.php

--- app/ring_groups/app_config.php
+++ app/ring_groups/app_config.php
@@ -44,6 +44,7 @@
 		$apps[$x]['destinations'][$y]['field']['context'] = "ring_group_context";
 		$apps[$x]['destinations'][$y]['select_value']['dialplan'] = "transfer:\${destination} XML \${context}";
 		$apps[$x]['destinations'][$y]['select_value']['ivr'] = "menu-exec-app:transfer \${destination} XML \${context}";
+		$apps[$x]['destinations'][$y]['select_value']['simple'] = "\${destination}";
 		$apps[$x]['destinations'][$y]['select_label'] = "\${destination} \${name} \${description}";
 
 	//cache details
