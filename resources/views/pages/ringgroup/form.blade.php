f807e740a38716ab973b214232425ceb6dd41a1f
Agustin Martin <agu.m.gg@gmail.com>
2025-10-15 15:56:36 -0300

    fix ring-group

--- resources/views/pages/ringgroup/form.blade.php
+++ resources/views/pages/ringgroup/form.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<livewire:ringgroup-form :ringGroupUuid="$ringGroupUuid ?? null"/>
+<livewire:ring-group-form :ringGroupUuid="$ringGroupUuid ?? null"/>
 @endsection
