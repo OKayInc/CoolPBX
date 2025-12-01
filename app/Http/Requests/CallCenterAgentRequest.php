7cc9151bb5e5e2101c24c6b5516f2cd9f0f0816b
Luis Daniel Lucio Quiroz <dieu@andres.okay.com.mx>
2025-12-01 15:01:46 -0500

    bad rule

--- app/Http/Requests/CallCenterAgentRequest.php
+++ app/Http/Requests/CallCenterAgentRequest.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests;
 
 use App\Models\CallCenterAgent;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\Rule;
 
