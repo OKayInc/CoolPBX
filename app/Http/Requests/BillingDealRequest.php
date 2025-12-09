4d5a206660a1d71769ef562ea6c21819120d88d6
Luis Daniel Lucio Quiroz <dieu@andres.okay.com.mx>
2025-12-09 12:07:01 -0500

    Billing deal rules

--- app/Http/Requests/BillingDealRequest.php
+++ app/Http/Requests/BillingDealRequest.php
@@ -17,15 +17,15 @@ class BillingDealRequest extends FormRequest
 		return [
 			'label' => 'bail|required|string',
 			'direction' => 'bail|required|in:outbound,inbound,local',
-			'digits' => 'bail|nullable|string',
-			'minutes' => 'bail|nullable|numeric|min:1',
-			'rate' => 'bail|nullable|numeric|min:0',
+			'digits' => 'bail|nullable|numeric|integer|min:1',
+			'minutes' => 'bail|required|numeric|integer|min:1',
+			'rate' => 'bail|required|numeric|min:0',
 			'billing_deal_notes' => 'bail|nullable|string|max:255',
             'currency' => [
                 'bail',
                 'nullable',
                 'string',
-                Rule::in(array_merge(config('currencies'), ['%']))
+                Rule::in(array_merge(config('currencies'), [])),
             ],
 		];
 	}
