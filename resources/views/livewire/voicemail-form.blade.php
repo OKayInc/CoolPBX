ee6b0604449ad03646247f337995868c75f79d09
root <root@chi-pbx-dev.hornblower.com>
2026-01-02 18:32:41 +0000

    error bags

--- resources/views/livewire/voicemail-form.blade.php
+++ resources/views/livewire/voicemail-form.blade.php
@@ -580,4 +580,11 @@
             @endif
         </script>
     @endpush
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
 </div>
