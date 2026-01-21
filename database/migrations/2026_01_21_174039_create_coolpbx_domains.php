cba46abb3045cb4af97ab62b172abb5f75df8981
Luis Daniel Lucio Quiroz <dieu@andres.okay.com.mx>
2026-01-21 14:16:24 -0500

    domain migrations

---
+++ database/migrations/2026_01_21_174039_create_coolpbx_domains.php
@@ -0,0 +1,80 @@
+<?php
+
+use App\Models\Domain;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $tableName = User::getTableName();
+        if (!Schema::hasTable($tableName))
+        {
+            Schema::create($tableName, function (Blueprint $table)
+            {
+                $table->collation('latin1_swedish_ci');
+                switch (env('DB_CONNECTION', 'mysql'))
+                {
+                    case 'mariadb':
+                    case 'mysql':
+                        $table->collation('latin1_swedish_ci');
+                        $table->engine('InnoDB');
+                        break;
+                    case 'pgsql':
+                        $table->collation('en_US.utf8');
+                }
+                $table->uuid('domain_uuid')->primary();
+                $table->uuid('domain_uuid')->nullable();
+                $table->string('domain_name', length: 255)->nullable(false);
+                $table->enum('domain_enabled', ['true','false'])->nullable(false);
+                $table->text('domain_description', length: 255)->nullable(false);
+                // timestamps
+                $table->date('insert_date')->nullable();
+                $table->uuid('insert_user')->nullable();
+                $table->date('update_date')->nullable();
+                $table->uuid('update_user')->nullable();
+            });
+        }
+        else
+        {
+            Schema::table($tableName, function (Blueprint $table) {
+                $table->collation('latin1_swedish_ci');
+                switch (env('DB_CONNECTION', 'mysql'))
+                {
+                    case 'mariadb':
+                    case 'mysql':
+                        $table->collation('latin1_swedish_ci');
+                        $table->engine('InnoDB');
+                        break;
+                    case 'pgsql':
+                        $table->collation('en_US.utf8');
+                }
+                $table->uuid('domain_uuid')->primary();
+                $table->uuid('domain_uuid')->nullable();
+                $table->string('domain_name', length: 255)->nullable(false);
+                $table->enum('domain_enabled', ['true','false'])->nullable(false);
+                $table->text('domain_description', length: 255)->nullable(false);
+                // timestamps
+                $table->date('insert_date')->nullable();
+                $table->uuid('insert_user')->nullable();
+                $table->date('update_date')->nullable();
+                $table->uuid('update_user')->nullable();
+            }
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $tableName = User::getTableName();
+        Schema::dropIfExists($tableName);
+    }
+};
