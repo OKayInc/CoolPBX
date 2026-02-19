ba0c890661f0a49667f596d7f920402500555a6b
Luis Daniel Lucio Quiroz <dieu@andres.okay.com.mx>
2026-02-19 14:41:34 -0500

    new models and migrations

---
+++ database/migrations/2026_02_19_193633_create_coolpbx_sip_profile_domains.php
@@ -0,0 +1,66 @@
+<?php
+
+use App\Models\SipProfileDomain;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $tableName = SipProfileDomain::getTableName();
+        if (!Schema::hasTable($tableName))
+        {
+            Schema::create($tableName, function (Blueprint $table)
+            {
+                $this->dql($table);
+            });
+        }
+        else
+        {
+            Schema::table($tableName, function (Blueprint $table) {
+                $this->dql($table);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $tableName = SipProfileDomain::getTableName();
+        Schema::dropIfExists($tableName);
+    }
+
+    private function dql(Blueprint $table): void
+    {
+        switch (env('DB_CONNECTION', 'mysql'))
+        {
+            case 'mariadb':
+            case 'mysql':
+                $table->collation('unicode_ci');
+                $table->engine('InnoDB');
+                break;
+            case 'pgsql':
+                $table->collation('en_US.utf8');
+        }
+
+        $table->uuid('sip_profile_domain_uuid')->nullable(false)->primary()->first();
+        $table->uuid('sip_profile_uuid')->nullable(false);
+        $table->string('sip_profile_domain_name', length: 255)->nullable(false);
+        $table->enum('sip_profile_domain_alias', ['true','false'])->default('false')->nullable(false);
+        $table->enum('sip_profile_domain_parse', ['true','false'])->default('false')->nullable(false);
+
+        // timestamps
+        $table->date('insert_date')->nullable();
+        $table->uuid('insert_user')->nullable();
+        $table->date('update_date')->nullable();
+        $table->uuid('update_user')->nullable();
+        $table->comment('CoolPBX setting information');
+    }
+};
