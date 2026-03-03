5e6bc6a5c4f0a6f7df55632be17db48fc9a59026
Luis Daniel Lucio Quiroz <luis.daniel.lucio@gmail.com>
2026-03-03 10:13:45 -0500

    Handle null case for record_type in xml_cdr.php

--- app/xml_cdr/resources/classes/xml_cdr.php
+++ app/xml_cdr/resources/classes/xml_cdr.php
@@ -490,7 +490,7 @@ if (!class_exists('xml_cdr')) {
 						//$this->array[$key]['digits_dialed'] = urldecode($xml->variables->digits_dialed);
 						$this->array[$key]['sip_hangup_disposition'] = urldecode($xml->variables->sip_hangup_disposition);
 						$this->array[$key]['pin_number'] = urldecode($xml->variables->pin_number);
-						$this->array[$key]['record_type'] = isset($xml->variables->record_type)?urldecode($xml->variables->record_type):($_REQUEST['record_type']?'call');
+						$this->array[$key]['record_type'] = isset($xml->variables->record_type)?urldecode($xml->variables->record_type):($_REQUEST['record_type']?'call':null);
 
 					//time
 						$start_epoch = urldecode($xml->variables->start_epoch);
